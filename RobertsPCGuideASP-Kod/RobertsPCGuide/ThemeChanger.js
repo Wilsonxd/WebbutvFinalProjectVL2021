eb192c1d9283942e020b79424162cf16d699277d
Vilhelm Lindahl <vilhelm.lindahl@edu.jonkoping.se>
2021-05-16 20:56:22 +0200

    Final Version

--- RobertsPCGuideASP-Kod/RobertsPCGuide/ThemeChanger.js
+++ RobertsPCGuideASP-Kod/RobertsPCGuide/ThemeChanger.js
@@ -1,6 +1,7 @@
-﻿function changeCSS(cssFile, cssLinkIndex) {
-
+﻿/*En funktion som byter hemsidan mellan två
+ olika css filer. I detta fallet mellan Light.css 
+ och dark.css*/
+function changeCSS(cssFile, cssLinkIndex) {
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-
     oldlink.href = cssFile;
 }
--- RobertsPCGuideASP-Kod/RobertsPCGuide/cart.js
+++ RobertsPCGuideASP-Kod/RobertsPCGuide/cart.js
@@ -1,6 +1,6 @@
 ﻿var total = 0;
 function add2Cart(namn, pris) {
-
+    /*Skapar varukorgen*/
     var tabell = document.getElementById("TableCart");
     var rad = tabell.insertRow();
     var cell1 = rad.insertCell();
@@ -11,27 +11,25 @@ function add2Cart(namn, pris) {
     document.getElementById("purchaseInfo").innerHTML = "Total: " + "$" + total;
 }
 
+/*Visar tabellen när man lägger något i varukorgen*/
 function displayTable() {
     document.getElementById('hideShow').style.display = "block";
 }
 
+/*Rensar varukorgen*/
 function Rensa() {
     if (confirm("Are you sure you want to clear your cart?")) {
         total = 0;
         document.getElementById("purchaseInfo").innerHTML = "Total: " + "$" + total;
         var tabell = document.getElementById("TableCart");
-        tabell.innerHTML = "<tr><td>Name</td><td>Pris</td></tr>";
+        tabell.innerHTML = "<tr><td>Name</td><td>Price</td></tr>";
         if (total == 0) {
             document.getElementById('hideShow').style.display = "none";
         }
     } else { }
 }
 
-var ix = 0;
-function increaseNum() {
-    document.getElementById('increase').value = ++ix;
-}
-
+/*Ger alert meddelande när man är klar*/
 function checkOut() {
     alert("Your order has been sent! Thank you!")
 }
